dde4fa5d70e9793fd7ef06b7eb0bf0f30d3b09a5
jennyli1299 <jennyli1299@users.noreply.github.com>
2020-11-02 23:03:20 -0500

    strange

--- app/src/main/java/com/example/sse/customlistview_sse/MainActivity.java
+++ app/src/main/java/com/example/sse/customlistview_sse/MainActivity.java
@@ -138,8 +138,8 @@ public class MainActivity extends AppCompatActivity {
          */
         if(id == R.id.menu_kahn){
             Log.i("TAG", "Kahn clicked");
-            Intent intent = new Intent(MainActivity.this, KahnActivity.class);
-            MainActivity.this.startActivity(intent);
+//            Intent intent = new Intent(MainActivity.this, KahnActivity.class);
+//            MainActivity.this.startActivity(intent);
         }
 
         return super.onOptionsItemSelected(item);  //if none of the above are true, do the default and return a boolean.
